fa8c35f6253722bd3146d4f500738b0a749ff434
TeniG <tenigada@gmail.com>
2023-05-25 20:04:10 +0530

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/categories_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const App());
+}
+
+final mytheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 203, 64, 94),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: mytheme, home: CategoriesScreen());
+  }
+}
